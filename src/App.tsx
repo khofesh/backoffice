9d32e75078550730e388102d932b188558ee5bce
fahmi ahmad <fahmiahmad@tutanota.com>
2020-04-02 18:32:54 +0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button } from "@blueprintjs/core";
 
 function App() {
+  let [someNumber, setSomeNumber] = useState(0);
+
+  const incrementCounter = () => {
+    setSomeNumber(someNumber + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: 5 }}>
+      <div>{someNumber}</div>
+
+      <div>
+        <Button intent="success" text="increase" onClick={incrementCounter} />
+      </div>
     </div>
   );
 }
---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+const Login: FunctionComponent = () => {
+  return (
+    <div>
+      <p>login page</p>
+    </div>
+  );
+};
+
+export default Login;
