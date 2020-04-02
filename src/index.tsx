fa5f3eb0cffe20bde632aef372a5bdf2ecdc973c
fahmi ahmad <fahmiahmad@tutanota.com>
2020-04-02 22:33:03 +0700

    set redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/css/index.css";
-import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import * as serviceWorker from "./serviceWorker";
 
+import { reducers } from "./reducers";
+
+import App from "./App";
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+
   document.getElementById("root")
 );
 
