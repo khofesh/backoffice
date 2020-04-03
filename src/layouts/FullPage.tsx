6480d2fa801e28e2c8d49a8e970e168d3716b9a7
fahmi ahmad <fahmiahmad@tutanota.com>
2020-04-03 11:49:47 +0700

    update

--- src/layouts/FullPage.tsx
+++ src/layouts/FullPage.tsx
@@ -3,7 +3,6 @@ import React, { FunctionComponent } from "react";
 const FullPage: FunctionComponent = props => {
   return (
     <div>
-      fullpage layout
       <main>
         <div>{props.children}</div>
       </main>
