988cade20b7a31bb994d2b11b816bf7d91ac5996
fahmi ahmad <fahmiahmad@tutanota.com>
2020-04-03 12:49:23 +0700

    add several components to signup page and update signup link on login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -103,7 +103,7 @@ const Login: FunctionComponent<RouteComponentProps> = () => {
               </Link>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="signup" variant="body2">
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
