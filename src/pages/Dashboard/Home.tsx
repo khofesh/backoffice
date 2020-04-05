9fdb5597ccc195e2e63c047b0c9dc11204885eed
fahmi ahmad <fahmiahmad@tutanota.com>
2020-04-05 22:20:05 +0700

    home for dashboard

---
+++ src/pages/Dashboard/Home.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from "react";
+import { RouteComponentProps } from "@reach/router";
+
+const DashboardHome: FunctionComponent<RouteComponentProps> = () => {
+  return (
+    <div style={{ margin: 10 }}>
+      <p>dashboard home</p>
+    </div>
+  );
+};
+
+export default DashboardHome;
