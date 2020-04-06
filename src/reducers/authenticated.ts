db6d48f8c41c1554af23155f57737f0d5326cb70
fahmi ahmad <fahmiahmad@tutanota.com>
2020-04-06 11:20:36 +0700

    authenticated reducer

---
+++ src/reducers/authenticated.ts
@@ -0,0 +1,18 @@
+import {
+  AuthenticatedInterface,
+  AuthenticatedAction
+} from "../actions/interfaces";
+import { ActionTypes } from "../actions/types";
+
+export const AuthenticatedReducer = (
+  state: AuthenticatedInterface = { authenticated: false },
+  action: AuthenticatedAction
+) => {
+  switch (action.type) {
+    case ActionTypes.authenticated:
+      return action.payload;
+
+    default:
+      return state;
+  }
+};
