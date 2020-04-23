791bfa7088e53b5ff6997cbc5d80952ca3eada05
fahmi ahmad <fahmiahmad@tutanota.com>
2020-04-23 19:05:41 +0700

    add new prop and move styles into styles.ts

--- src/components/navigations/TopBar.tsx
+++ src/components/navigations/TopBar.tsx
@@ -1,10 +1,4 @@
 import React, { FunctionComponent } from "react";
-import {
-  createStyles,
-  makeStyles,
-  Theme,
-  fade,
-} from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -19,75 +13,9 @@ import AccountCircle from "@material-ui/icons/AccountCircle";
 import MailIcon from "@material-ui/icons/Mail";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import MoreIcon from "@material-ui/icons/MoreVert";
+import clsx from "clsx";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    grow: {
-      flexGrow: 1,
-    },
-    toolbar: {
-      backgroundColor: "#1976D2",
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      display: "none",
-      [theme.breakpoints.up("sm")]: {
-        display: "block",
-      },
-    },
-    search: {
-      position: "relative",
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      "&:hover": {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-      },
-      marginRight: theme.spacing(2),
-      marginLeft: 0,
-      width: "100%",
-      [theme.breakpoints.up("sm")]: {
-        marginLeft: theme.spacing(3),
-        width: "auto",
-      },
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: "100%",
-      position: "absolute",
-      pointerEvents: "none",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    inputRoot: {
-      color: "inherit",
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("md")]: {
-        width: "20ch",
-      },
-    },
-    sectionDesktop: {
-      display: "none",
-      [theme.breakpoints.up("md")]: {
-        display: "flex",
-      },
-    },
-    sectionMobile: {
-      display: "flex",
-      [theme.breakpoints.up("md")]: {
-        display: "none",
-      },
-    },
-  })
-);
+import { topbarStyles } from "./styles";
 
 interface TopBarProps {
   anchorEl: null | HTMLElement;
@@ -98,6 +26,8 @@ interface TopBarProps {
   handleMobileMenuClose: () => void;
   handleMenuClose: () => void;
   handleMobileMenuOpen: (event: React.MouseEvent<HTMLElement>) => void;
+  handleDrawerOpen: () => void;
+  isDrawerOpen?: boolean;
 }
 
 const TopBar: FunctionComponent<TopBarProps> = ({
@@ -109,8 +39,10 @@ const TopBar: FunctionComponent<TopBarProps> = ({
   handleMobileMenuClose,
   handleMenuClose,
   handleMobileMenuOpen,
+  handleDrawerOpen,
+  isDrawerOpen,
 }) => {
-  const classes = useStyles();
+  const classes = topbarStyles();
 
   const menuId = "primary-search-account-menu";
   const renderMenu = (
@@ -171,13 +103,19 @@ const TopBar: FunctionComponent<TopBarProps> = ({
 
   return (
     <div className={classes.grow}>
-      <AppBar position="static">
+      <AppBar
+        position="static"
+        className={clsx(classes.toolbar, {
+          [classes.toolbarShift]: isDrawerOpen,
+        })}
+      >
         <Toolbar className={classes.toolbar}>
           <IconButton
             edge="start"
-            className={classes.menuButton}
+            className={clsx(classes.menuButton, isDrawerOpen && classes.hide)}
             color="inherit"
             aria-label="open drawer"
+            onClick={handleDrawerOpen}
           >
             <MenuIcon />
           </IconButton>
