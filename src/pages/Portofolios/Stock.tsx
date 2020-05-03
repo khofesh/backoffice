810e123614b106b1f4548756eefb29bac35910a3
fahmi ahmad <fahmiahmad@tutanota.com>
2020-05-03 22:55:57 +0700

    add a table

--- src/pages/Portofolios/Stock.tsx
+++ src/pages/Portofolios/Stock.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from "react";
 import { RouteComponentProps } from "@reach/router";
+import { DataSheet } from "../../components/tables/DataSheet";
 
 interface StockProps extends RouteComponentProps {
   stockId?: string;
@@ -8,6 +9,7 @@ const Stock: FunctionComponent<StockProps> = (props) => {
   return (
     <div>
       <h2>stock {props.stockId}</h2>
+      <DataSheet />
     </div>
   );
 };
