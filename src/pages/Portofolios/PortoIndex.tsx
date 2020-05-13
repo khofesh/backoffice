2a54a652f288a37f1508c41e9d08aaa8b4ec73a9
fahmi ahmad <fahmiahmad@tutanota.com>
2020-05-13 21:57:05 +0700

    tidy up a little bit with grid

--- src/pages/Portofolios/PortoIndex.tsx
+++ src/pages/Portofolios/PortoIndex.tsx
@@ -1,10 +1,16 @@
 import React, { FunctionComponent } from "react";
-import { RouteComponentProps } from "@reach/router";
+import { Link, RouteComponentProps } from "@reach/router";
+import Grid from "@material-ui/core/Grid";
 
 const InvoicesIndex: FunctionComponent<RouteComponentProps> = () => (
-  <div>
-    <p>Maybe put some pretty graphs here or something.</p>
-  </div>
+  <Grid container spacing={3} justify="center">
+    <Grid item xs={12} md={8} lg={11} xl={11}>
+      <div style={{ marginBottom: 5 }}>
+        <Link to="123">stock 1</Link> <Link to="124">stock 2</Link>
+      </div>
+      <p>Maybe put some pretty graphs here or something.</p>
+    </Grid>
+  </Grid>
 );
 
 export default InvoicesIndex;
