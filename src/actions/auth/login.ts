352551c5189ba9979463aba60784cd297cc6b1eb
fahmi ahmad <fahmiahmad@tutanota.com>
2020-05-27 23:44:19 +0700

    move loginAction to login.ts

---
+++ src/actions/auth/login.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { Dispatch } from "redux";
+import { ActionTypes } from "../types";
+import { LoginResponse } from "../interfaces";
+import { navigate } from "@reach/router";
+
+/**
+ *
+ * @param email string fahmi@backoffice.com
+ * @param password string something88888
+ */
+export const loginAction = (email: string, password: string) => {
+  return async (dispatch: Dispatch) => {
+    const response = await axios.post<LoginResponse>(
+      "http://localhost:8090/api/v0/admin/login",
+      {
+        email,
+        password,
+      },
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      }
+    );
+
+    if (response.data.access_token) {
+      dispatch({
+        type: ActionTypes.authenticated,
+        payload: { status: true },
+      });
+      navigate("/");
+    }
+    dispatch({
+      type: ActionTypes.login,
+      payload: response.data,
+    });
+  };
+};
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,34 +1,8 @@
-import axios from "axios";
-import { Dispatch } from "redux";
-import { ActionTypes } from "./types";
-import { LoginInterface } from "./interfaces";
-import { navigate } from "@reach/router";
+import { loginAction } from "./auth/login";
 
 /**
  *
  * @param email string eve.holt@reqres.in
  * @param password string cityslicka
  */
-export const loginAction = (email: string, password: string) => {
-  return async (dispatch: Dispatch) => {
-    const response = await axios.post<LoginInterface>(
-      "https://reqres.in/api/login",
-      {
-        email,
-        password
-      }
-    );
-
-    if (response.data.token) {
-      dispatch({
-        type: ActionTypes.authenticated,
-        payload: { status: true }
-      });
-      navigate("/");
-    }
-    dispatch({
-      type: ActionTypes.login,
-      payload: response.data
-    });
-  };
-};
+export { loginAction };
