8392a3666d4ac5b5a9bd203ccf6a8fd6aa6ac436
fahmi ahmad <fahmiahmad@tutanota.com>
2020-06-01 20:40:04 +0700

    logout action

---
+++ src/actions/auth/logout.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { Dispatch } from "redux";
+import { ActionTypes } from "../types";
+import { LogoutSuccessResponse } from "../interfaces";
+import { navigate } from "@reach/router";
+
+export const logoutAction = () => {
+  return async (dispatch: Dispatch) => {
+    const response = await axios.get<LogoutSuccessResponse>(
+      "http://localhost:8090/api/v0/admin/logout",
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      }
+    );
+
+    if (response?.data?.code === 200) {
+      dispatch({
+        type: ActionTypes.logout,
+        payload: { logoutSuccess: true },
+      });
+      dispatch({
+        type: ActionTypes.login,
+        payload: {
+          access_token: "",
+          access_expire: "",
+          refresh_token: "",
+          refresh_expire: "",
+          userData: {
+            id: "",
+            email: "",
+            role: "",
+          },
+        },
+      });
+      dispatch({
+        type: ActionTypes.authenticated,
+        payload: { status: false },
+      });
+      navigate("/login");
+    }
+  };
+};
