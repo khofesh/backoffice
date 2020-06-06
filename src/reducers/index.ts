0bea131889da000f181bb01fa11e26ba4a823da9
fahmi ahmad <fahmiahmad@tutanota.com>
2020-06-06 11:44:53 +0700

    register logout and check-session reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -5,8 +5,12 @@ import {
   AuthenticatedInterface,
   DrawerInterface,
   TempDrawerInterface,
+  SessionInterface,
+  LogoutInterface,
 } from "../actions/interfaces";
 import { AuthenticatedReducer } from "./authenticated";
+import { checkSessionReducer } from "./auth/session";
+import { logoutReducer } from "./auth/logout";
 import { DrawerReducer, TempDrawerReducer } from "./navigations/drawer";
 
 export interface StoreState {
@@ -14,10 +18,14 @@ export interface StoreState {
   authenticated: AuthenticatedInterface;
   drawer: DrawerInterface;
   tempdrawer: TempDrawerInterface;
+  checkSession: SessionInterface;
+  logout: LogoutInterface;
 }
 export const reducers = combineReducers<StoreState>({
   login: loginReducer,
   authenticated: AuthenticatedReducer,
   drawer: DrawerReducer,
   tempdrawer: TempDrawerReducer,
+  checkSession: checkSessionReducer,
+  logout: logoutReducer,
 });
