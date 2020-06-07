224f5e99d5fba5de5a0114aa5e184ed2faa9f921
fahmi ahmad <fahmiahmad@tutanota.com>
2020-06-07 12:55:41 +0700

    add props

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,13 +1,25 @@
 import React, { FunctionComponent } from "react";
 import { RouteComponentProps, Link } from "@reach/router";
 
-const NotFound: FunctionComponent<RouteComponentProps> = () => {
+interface NotFoundProps extends RouteComponentProps {
+  routePath?: string;
+  routeName?: string;
+}
+const NotFound: FunctionComponent<NotFoundProps> = ({
+  routeName,
+  routePath,
+}) => {
   return (
     <div style={{ margin: 10 }}>
       <p>Sorry, nothing here</p>
-      <Link to="/">Home</Link>
+      <Link to={routePath ? routePath : "/"}>{routeName}</Link>
     </div>
   );
+};
+
+NotFound.defaultProps = {
+  routePath: "/",
+  routeName: "Home",
 };
 
 export default NotFound;
