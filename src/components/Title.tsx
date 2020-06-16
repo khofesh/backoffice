bcc6f62d0c5b1b2aa043d0b0aa011543fbbaa607
fahmi ahmad <fahmiahmad@tutanota.com>
2020-06-16 10:07:17 +0700

    remove bottom gutter

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ import Typography from "@material-ui/core/Typography";
 
 const Title: React.FC = (props) => {
   return (
-    <Typography component="h2" variant="h6" color="primary" gutterBottom>
+    <Typography component="h2" variant="h6" color="primary">
       {props.children}
     </Typography>
   );
