a61a23e0e6f7b24e4ba86c2f6061b86efc7a6ed8
fahmi ahmad <fahmiahmad@tutanota.com>
2020-06-16 10:06:57 +0700

    add dialog

---
+++ src/components/stocks/TradeScenarioDialog.tsx
@@ -0,0 +1,57 @@
+import React, { FunctionComponent } from "react";
+import Button from "@material-ui/core/Button";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+import Dialog from "@material-ui/core/Dialog";
+
+interface TradeScenarioDialogProps {
+  classes?: Record<"paper", string>;
+  id: string;
+  keepMounted: boolean;
+  open: boolean;
+  onClose: () => void;
+  title: string;
+}
+
+const TradeScenarioDialog: FunctionComponent<TradeScenarioDialogProps> = (
+  props
+) => {
+  const { onClose, open, title, children, ...other } = props;
+
+  const handleCancel = () => {
+    onClose();
+  };
+
+  const handleOk = () => {
+    onClose();
+  };
+
+  return (
+    <>
+      <Dialog
+        open={open}
+        disableBackdropClick
+        disableEscapeKeyDown
+        maxWidth="xs"
+        aria-labelledby="confirmation-dialog-title"
+        {...other}
+      >
+        <DialogTitle id="confirmation-dialog-title">{title}</DialogTitle>
+
+        <DialogContent dividers>{children}</DialogContent>
+
+        <DialogActions>
+          <Button autoFocus onClick={handleCancel} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleOk} color="primary">
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default TradeScenarioDialog;
